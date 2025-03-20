9ae637109a00a3dca57bf59b3f764692fca6d3d5
snow <itr802222@gmail.com>
2025-03-20 10:00:24 +0800

    10428_origin_version

---
+++ 10428.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(){
+        int MaxN = 75000;
+        uint64_t arr[MaxN];
+        int n;
+        scanf("%d", &n);
+ 
+        for(int i=0;i<MaxN;i++){
+                scanf("%llu", &arr[i]);
+        }
+ 
+        // row
+        int row_n = 0;
+        for(int i=0;i<MaxN;i++){
+                if(n>64) break;
+ 
+                uint64_t tmp = (1ULL<<n)-1;
+                for(int j=0;j<65-n;j++){
+                        uint64_t now = tmp<<j ;
+                        if( (arr[i]&now) == now )  row_n++;
+                }
+        }
+        printf("%d\n", row_n);
+ 
+        // column
+        int col_n = 0;
+        for(int i=0;i<MaxN-n+1;i++){
+                uint64_t tot = arr[i];
+                for(int k=1;k<n;k++){
+                        tot = tot&arr[i+k];
+                }
+                //printf("%d %llu\n", i, tot);
+ 
+                for(int j=0;j<64;j++){
+                        uint64_t tmp = 1ULL<<j;
+ 
+                        if( (tmp&tot)!=0 ) col_n++;
+                }
+ 
+        }
+        printf("%d\n",col_n);
+ 
+        // dia
+        int dia_n = 0;
+        for(int i=0;i<MaxN-n+1;i++){
+                uint64_t tot = arr[i]>>(n-1);
+                for(int k=1;k<n;k++){
+                        tot &= arr[i+k]>>(n-1-k);
+                }
+ 
+                for(int j=0;j<65-n;j++){
+                        uint64_t tmp = 1ULL<<j;
+ 
+                        if( (tmp&tot)!=0 ) dia_n++;
+                }
+        }
+        printf("%d\n",dia_n);
+ 
+        // anti-dia
+        int adia_n = 0;
+        for(int i=0;i<MaxN-n+1;i++){
+                uint64_t tot = arr[i];
+                for(int k=1;k<n;k++){
+                        tot &= arr[i+k]>>k;
+                }
+ 
+                for(int j=0;j<65-n;j++){
+                        uint64_t tmp = 1ULL<<j;
+ 
+                        if( (tmp&tot)!=0 ) adia_n++;
+                }
+        }
+        printf("%d\n",adia_n);
+}
